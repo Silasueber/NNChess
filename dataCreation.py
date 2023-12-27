4d592099b7e463128f4ee6a459cd3b71e4d7b6ed
Deischox <silas@vitamoment.de>
2023-12-27 12:51:11 +0100

    Viele sachen

--- dataCreation.py
+++ dataCreation.py
@@ -9,7 +9,7 @@ stockfish_white.set_elo_rating(1000)
 stockfish_black = Stockfish()
 # stockfish_black.set_elo_rating(1350)
 
-csv_path = "positions.csv"
+
 
 # Play moves
 def playMove(move):
@@ -33,11 +33,13 @@ def convertPositionToString(board):
     return ','.join(result)
 
 
-# two neurons for determine whose turn it is white->(1,0) black->(0,1) 
+
 def createDataEntry(whitesTurn):
+    # Dataset one with 1 -> White winning 0 -> Black winning 0.5 -> Draw
+    csv_path = "data/p1.csv"
     position = convertPositionToString(stockfish_black.get_board_visual())
     eval = convertEvalIntoValue()
-    turn = "1,0," if whitesTurn else "0,1,"
+    turn = "1," if whitesTurn else "0,"
     line = turn+position+","+str(eval)
     try:
         with open(csv_path, 'a', newline='') as csv_file:
@@ -46,6 +48,35 @@ def createDataEntry(whitesTurn):
     except Exception as e:
         print(f"Error: {e}")
 
+    # Dataset two with [1,0] -> White winning [0,1] -> Black winning [1,1] -> Draw
+    csv_path = "data/p2.csv"
+    eval = convertEvalIntoValue()
+    winner = ""
+    if eval == 1:
+        winner = "1,0,0"
+    elif eval == 0: 
+        winner = "0,1,0"
+    else:
+        winner = "0,0,1"
+
+    line = turn+position+","+winner
+    try:
+        with open(csv_path, 'a', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(line.split(','))
+    except Exception as e:
+        print(f"Error: {e}")
+
+    # Dataset three with cpawn value for position [limited at -10 and +10]
+    csv_path = "data/p3.csv"
+    winner = getCpawnValue()
+    line = turn+position+","+str(winner)
+    try:
+        with open(csv_path, 'a', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(line.split(','))
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def convertEvalIntoValue():
@@ -59,25 +90,32 @@ def convertEvalIntoValue():
             return 1
         else: 
             # Winning Black
-            return -1
+            return 0
     else:
         if eval_value > 100: 
             # Winning white
             return 1
         elif eval_value < -100: 
             # Winning Black
-            return -1
+            return 0
         else:
             # Equal
-            return 0
+            return 0.5
     
-    # Can be used later for exact values
-        
-    # if eval.get('type') == "cp":
-    #     print(eval.get('value'))
-    # else:
-    #     print("Mate in: ",eval.get('value'))
-    #     print(eval)
+def getCpawnValue():
+    # Be careful: the stockfish that evaluats must be always the best possbile version, if stockfish black is not the best change this line
+    eval = stockfish_black.get_evaluation()
+    eval_type = eval.get('type')
+    eval_value = eval.get("value")
+    if eval_type == "mate":
+        if eval_value > 0: 
+            return 10000-(eval_value*1000)
+        else: 
+            # Winning Black
+            return -10000+(eval_value*1000)
+    else:
+        return eval_value
+   
 
 def playGame():
     while stockfish_white.get_best_move():
@@ -107,8 +145,8 @@ def playGameLimitedMoves(moves):
             createDataEntry(whitesTurn=False)
             playMove(stockfish_black.get_best_move_time(100))
             moves -= 1
-        except:
-            print("Couldnt make move")
+        except Exception as e:
+            print(e)
             moves = 0
     #print(stockfish_white.get_board_visual())
 
@@ -116,6 +154,7 @@ def createRandomFen(min_moves=10, max_moves=30):
     board = chess.Board()
     num_moves = random.randint(min_moves, max_moves)
     for i in range(num_moves):
+        if board.is_game_over(): break
         try:
             legal_moves = [move for move in board.legal_moves]
             random_move = random.choice(legal_moves)
@@ -134,5 +173,5 @@ def setPosition(position):
 amount_of_games = 100
 for i in range(amount_of_games):
     print("Current Game: " + str(i))
-    setPosition(createRandomFen(min_moves=100, max_moves=200))
-    playGameLimitedMoves(3)
+    setPosition(createRandomFen(min_moves=0, max_moves=200))
+    playGameLimitedMoves(100)
