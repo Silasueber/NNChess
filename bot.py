47893999ebaa4d90832b1578ed316776929c4a44
Benjamin Pöhlmann <benjamin_poehlmann@web.de>
2023-12-29 17:30:16 +0100

    1hot working for training

--- bot.py
+++ bot.py
@@ -7,20 +7,52 @@ from stockfishHelper import initalizeStockfish
 
 
 # load the dataset, split into input (X) and output (y) variables
-# BE CAREFUL the split must be changed if we use a different representation of who is winning cpawn vs, [1,0] 
+# BE CAREFUL the split must be changed if we use a different representation of who is winning cpawn vs, [1,0]
+one_hot_mapping = {
+    0: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Empty
+    1: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # White Pawn
+    3: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],  # White Knight/Bishop
+    5: [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],  # White Rook
+    10: [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],  # White Queen
+    1000: [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],  # White King
+    -1: [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],  # Black Pawn
+    -3: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],  # Black Knight/Bishop
+    -5: [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],  # Black Rook
+    -10: [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],  # Black Queen
+    -1000: [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]  # Black King
+}
+def transformSingleBoardToOneHot(board):
+    for field in board[1:]:
+        newBoardRepresentation = np.append(newBoardRepresentation, one_hot_mapping[field])
+def transformBoardsCsvToOneHot(boards):
 
-train = False
+    newBoardsRepresentation = np.array([])
+    for board in boards:
+        newBoardRepresentation = np.array([board[0]]) #dont wanna touch whos turn it is
+        for field in board[1:]:
+            newBoardRepresentation = np.append(newBoardRepresentation, one_hot_mapping[field])
+
+        newBoardsRepresentation = np.append(newBoardsRepresentation, newBoardRepresentation)
+
+    newBoardsRepresentation = newBoardsRepresentation.reshape(len(boards), 641) #641 = 1+64*10
+    return newBoardsRepresentation
+
+train = True
+modelName = "models/p2.pt"
+
 if train:
-    dataset = np.loadtxt('data/p2.csv', delimiter=',')
+    dataset = np.loadtxt('data/p4_one_hot.csv', delimiter=',') #TODO represent csv as one hot already?
     X = dataset[:, :65]
     y = dataset[:, 65:]
+    X = transformBoardsCsvToOneHot(X)
     # Convert to tensors
     X = torch.tensor(X, dtype=torch.float32)
     y = torch.tensor(y, dtype=torch.float32)
 
     # define model
+    noOfCpawnValues = 5
     model = nn.Sequential(
-        nn.Linear(65, 32),
+        nn.Linear(1 + 64 * noOfCpawnValues * 2, 32),
         nn.ReLU(),
         nn.Linear(32, 32),
         nn.ReLU(),
@@ -36,7 +68,7 @@ if train:
     batch_size = 100
     dataset = TensorDataset(X, y)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    n_epochs = 2000 
+    n_epochs = 1
     for epoch in range(n_epochs):
         for Xbatch, ybatch in dataloader:
             y_pred = model(Xbatch)
@@ -47,13 +79,12 @@ if train:
 
         # Print loss after each epoch
         print(f'Finished epoch {epoch}, latest loss {loss}')
-
-    torch.save(model, "models/p2.pt")
-    model = torch.load("models/p2.pt")
+    torch.save(model, modelName)
+    model = torch.load(modelName)
     model.eval()
 else:
     # LOAD OLD MODEL AND TRAIN ON IT
-    model = torch.load("models/p2.pt")
+    model = torch.load(modelName)
 
 whiteWinning = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 blackWinning = "5kq1/8/8/8/8/8/5K2/8 w - - 0 1"
@@ -81,6 +112,7 @@ def convertPositionToString(fen):
 def testFenPosition(fen):
     test = ("1,"+ convertPositionToString(fen)).split(",")
     test = [int(t) for t in test]
+    test = transformSingleBoardToOneHot(test)
     test = torch.tensor(test, dtype=torch.float32)
     predictions = model(test)
     print(predictions)
